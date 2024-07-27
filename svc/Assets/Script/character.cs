cd4d403cd52e838dea77f306bef9ee770891b389
selwjoo <moi080917@gmail.com>
2024-07-28 01:44:46 +0900

    플레이어 이동 수정

---
+++ svc/Assets/Script/character.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+   public float speed = 10f;
+       public float jumpForce = 5f;
+       private Rigidbody2D rb;
+       private bool isGrounded;
+    
+   
+       void Start()
+       {
+           rb = GetComponent<Rigidbody2D>();
+       }
+
+
+    void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+             
+        }
+    }
+
+}
