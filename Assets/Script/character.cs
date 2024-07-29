77ead5160465331131385bd1d96f28ebc8533d66
laeswo <129704273+laeswo@users.noreply.github.com>
2024-07-29 14:46:43 +0900

    stage1
    캐릭터가 점프했을때는 캐릭터에게 힘을 줄수 없도록함

--- Assets/Script/character.cs
+++ Assets/Script/character.cs
@@ -5,45 +5,64 @@ using UnityEngine;
 
 public class character : MonoBehaviour
 {
-   public float speed = 10f;
-       public float jumpForce = 5f;
-       private Rigidbody2D rb;
-       private bool isGrounded;
-    
-   
-       void Start()
-       {
-           rb = GetComponent<Rigidbody2D>();
-       }
+    public float speed = 10f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded;
 
-       private void OnCollisionStay2D(Collision2D other)
-       {
-           if (other.gameObject.tag == "Ground")
-           {
-               isGrounded = true;
-           }
-       }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
-       private void OnCollisionExit2D(Collision2D other)
-       {
-           if (other.gameObject.tag == "Ground")
-           {
-               isGrounded = false;
-           }
-       }
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
 
-       void Update()
-       {
-           Debug.Log(isGrounded);
-           float moveHorizontal = Input.GetAxis("Horizontal");
-   
-           Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
-           rb.AddForce(movement * speed);
-   
-           if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
-           {
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 
-               rb.AddForce(Vector3.up * jumpForce, (ForceMode2D)ForceMode.Impulse);
-           }
-       }
+    void Update()
+    {
+        Debug.Log(isGrounded);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        if (isGrounded)
+        {
+         
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+            rb.AddForce(movement * speed);
+
+        }
+       
+        if (moveHorizontal != 0)
+        {
+            animator.SetBool("walk", true);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+
+        if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
+        {
+            animator.SetBool("jump", true);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            animator.SetBool("jump", false);
+        }
+    }
 }
