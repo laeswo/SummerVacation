9327e2dd8b06b3d688d29d86cfb9da5dbebfe83f
selwjoo <moi080917@gmail.com>
2024-08-08 00:00:16 +0900

    스테이지 2까지

--- svc/Assets/Script/GameManager.cs
+++ svc/Assets/Script/GameManager.cs
@@ -8,29 +8,39 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
    
-
     public GameObject gameobj;
     public GameObject gameobj2;
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Instantiate(gameobj);
-
             Destroy(gameobj2);
         }
 
         if (collision.CompareTag("wall")){
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+
+        if (collision.CompareTag("Fake"))
+        {
+            Instantiate(gameobj);
+            Destroy(gameobj2);
+        }
+
+        
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Finish"))
         {
-            Debug.Log("³¡");
+            Destroy(gameobj);
+            Destroy(gameobj2);
         }
     }
+
+   
 }
