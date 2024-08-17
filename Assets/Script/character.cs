c350fc2f261a61ebe95e5e0912469f6a3611ffae
fluent0602 <fluent0602@gmail.com>
2024-08-17 19:24:57 +0900

    Update character.cs

--- Assets/Script/character.cs
+++ Assets/Script/character.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 
 public class character : MonoBehaviour
 {
-    public float speed = 10f;
-    public float jumpForce = 5f;
+    public float speed;
+    public float jumpForce = 6f;
     private Rigidbody2D rb;
+    private Animator animator;
     private bool isGrounded;
-
-
+    public bool delay;
+    private float time;
+    private int timer = 1;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        speed = 1f;
     }
 
     private void OnCollisionStay2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Ground")
+        if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
@@ -26,7 +30,7 @@ public class character : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Ground")
+        if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
         }
@@ -34,16 +38,45 @@ public class character : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(isGrounded);
+        Debug.Log(speed);
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
-        rb.AddForce(movement * speed);
+        if (isGrounded)
+        {
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+            rb.AddForce(movement * speed);
+
+        }
+
+        if (moveHorizontal != 0)
+        {
+            //animator.SetBool("walk", true);
+        }
+        else
+        {
+            // animator.SetBool("walk", false);
+        }
 
         if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
         {
+            // animator.SetBool("jump", true);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            //Wz` animator.SetBool("jump", false);
+        }
+        if (delay == true)
+        {
+            time += Time.deltaTime;
+            if (timer < time)
+            {
+                time = 0;
+                delay = false;
+                speed = 1;
 
-            rb.AddForce(Vector3.up * jumpForce, (ForceMode2D)ForceMode.Impulse);
+            }
         }
     }
 }
